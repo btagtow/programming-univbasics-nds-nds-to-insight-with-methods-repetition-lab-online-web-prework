5030dc5e889d56cf384d411f3ab658de5f54185c
Ben Tagtow <bentagtow@gmail.com>
2019-11-12 21:43:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -55,6 +55,6 @@ end
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-end
+
 
 
